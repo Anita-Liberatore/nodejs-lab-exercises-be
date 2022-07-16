f81ac0a537b65bcdab3285b4ff1f00fa70db7d64
Anita Liberatore <anitaliberatore@hotmail.com>
2022-07-16 19:47:32 +0200

    lab 5.1 done

--- lab5.1/index.js
+++ lab5.1/index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
+const model = require('./model');
 
+app.get('/boat/:id', (req, res) => {
+
+    model.boat.read(req.params.id, (err, data) => {
+
+        if (err) {
+
+            if (err.code == 'E_NOT_FOUND') {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(500);
+            }
+
+        } else {
+            res.send(data);
+        }
+
+    })
+});
+
+app.get('/ping', (req, res) => {
+
+    res.send("ping");
+
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT} - Lab 5.1 - Implement a RESTful JSON GET`));
