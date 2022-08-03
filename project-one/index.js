e52d41d52c487aa4a7403ac80584e331ec37800e
Anita Liberatore <anitaliberatore@hotmail.com>
2022-08-03 22:51:24 +0200

    add project one express training

---
+++ project-one/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const userController = require('./routes/user-controller');
+
+app.use('/', userController);
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT} - Project One`));
+
+module.exports = app
