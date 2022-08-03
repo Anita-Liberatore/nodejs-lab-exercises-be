0d439fe3e7ba5145f86f9ccfce9580ca89d4a849
Anita Liberatore <anitaliberatore@hotmail.com>
2022-08-03 23:16:18 +0200

    add json response to first route controller

--- project-one/index.js
+++ project-one/index.js
@@ -5,9 +5,6 @@ const userController = require('./routes/user-controller');
 
 app.use('/', userController);
 
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT} - Project One`));
 
--- project-one/routes/user-controller.js
+++ project-one/routes/user-controller.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send("Ok")
-  })
+  res.send(({ message: "Ping message"}))
+})
  
 module.exports = router;
