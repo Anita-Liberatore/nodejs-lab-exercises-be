698a778699151a34a53a55bb21f49993e8921208
Anita Liberatore <anitaliberatore@hotmail.com>
2022-08-25 22:53:54 +0200

    lab5.1

---
+++ labs-solution-alternative/lab5/lab5.1/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var model = require('./model')
+
+// respond with data from model.js using read function to get all data
+// Responds to HTTP GET requests to / with data
+app.get('/boat/:id', function(req, res, next) {
+  model.boat.read(req.params.id, (err, data) => {
+    console.log(req.params.id.length )
+    if(err) {
+        if(err.code === 'E_NOT_FOUND') {
+            next()
+            return
+        }
+
+        next(err)
+        return
+    }
+
+    if(req.params.id.length > 3) {
+        console.log("qui" )
+        return res.status(500).json({ message: "internal error" });
+    }
+
+    res.send(data)
+  })
+  
+});
+
+
+app.use((req, res) => {
+    if(req.method!='GET') {
+        return res.status(405).json({ message: "method not allowed" });
+    }
+    res.status(404).json({ message: "not found" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT} - Lab 5.1 - Implement a RESTful JSON GET`));
+
+module.exports = app
