aba45a1be15b99e0d2b65acdaeef8cbc9b4b6fc1
Anita Liberatore <anitaliberatore@hotmail.com>
2022-09-03 10:02:15 +0200

    add next(err)

--- other-exercise/exercise-three/index.js
+++ other-exercise/exercise-three/index.js
@@ -2,20 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-function toUpperCaseName(name = '') {
+function makeUpperCase(input = '') {
 
-    if(Array.isArray(name)) {
-        return name.map((c) => String(c).toUpperCase())
+    if(Array.isArray(input)) {
+        return input.map((c) => String(c).toUpperCase())
     }
-    return name.toUpperCase();
-}  
 
-app.get("/", (req, res) => {
-    setTimeout(() => {
-        const name = req.query.name
-        res.json(toUpperCaseName(name))
+    return input.toUpperCase();
+}
+app.get("/", (req, res, next, err) => {
+
+    if(!req.query.name) {
+       next(err)
+       return
+    }
+    setInterval(() => {
+        const id = req.query.name;
+        res.json(makeUpperCase(id))
     }, 300)
-    
+   
+
+})
+
+app.use((req, res) => {
+    return res.status(400).json({message: "bad request"})
 })
 
 app.listen(port, () => {
