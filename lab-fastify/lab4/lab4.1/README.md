5a32628a5edc35e2a1f0551321e10e119cf2c864
Anita Liberatore <anitaliberatore@hotmail.com>
2022-09-07 18:30:04 +0200

    lab4.1 passed

---
+++ lab-fastify/lab4/lab4.1/README.md
@@ -0,0 +1,10 @@
+Lab 4.1 - Render a View
+
+Using either Fastify or Express, create a project with a /me route. 
+
+- Render a view that uses the layout.hbs that was created in this chapter to create a small profile page. The HTML content
+is unimportant, just make sure to render a view.
+- The labs-1 folder contains a file named validate.js. 
+
+Make sure the server that the /me route was added to is running in another terminal, then with the current working directory set to the
+labs-1 folder, run the following to check the implementation: node validate
---
+++ lab-fastify/lab4/lab4.1/app.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const AutoLoad = require('fastify-autoload')
+const pointOfView = require('point-of-view')
+const handlebars = require('handlebars')
+
+module.exports = async function (fastify, opts) {
+  fastify.register(pointOfView, {
+    engine: { handlebars },
+    root: path.join(__dirname, 'views'),
+    layout: 'layout.hbs'
+  })
+
+  // Do not touch the following lines
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+}
